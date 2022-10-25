6be3985733b114817f5f6b43587a9c34d38cfcb2
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-10-25 19:31:12 +0600

    custom user model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,57 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.utils.translation import ugettext_lazy
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    # make email unique
+    def _create_user(self, email, password, **extra_fileds):
+
+        if not email:
+            raise ValueError('The email must be set!')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fileds)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        return self._create_user(email, password, **extra_fields)
+
+    
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True, null=False)
+    is_staff = models.BooleanField(
+        ugettext_lazy('staff status'),
+        default = False,
+        helf_text = ugettext_lazy('Designates whether the user can log in this site')
+    )
+
+    is_active = models.BooleanField(
+        ugettext_lazy('active'),
+        default = True,
+        help_text = ugettext_lazy('Designates whether this user should be treated as active. Unselect this instead of deleting accounts')
+    )
+
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
