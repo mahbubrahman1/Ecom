d3bcfb10eb42f49fb9318132a695060a280edbb8
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-10-27 18:59:29 +0600

    created views

--- order/urls.py
+++ order/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
+from .import views
+
 
 urlpatterns = [
-    
+    path('add/<str:pk>/', views.cart_add, name='add_cart'),
+    path('cart/', views.cart, name='cart'),
 ]
--- order/views.py
+++ order/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from shop.models import Product
+from .models import Cart, Order
+
+
+# add item in cart
+@login_required
+def cart_add(request, pk):
+    item = get_object_or_404(Product, pk=pk)
+    order_item = Cart.objects.get_or_create(item=item, user=request.user, purchased=False)
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+
+    if order_qs.exists():
+        order = order_qs[0]
+
+        if order.order_items.filter(item=item).exists():
+            order_item[0].quantity += 1
+            order_item[0].save()
+            messages.info(request, 'This item quantity was updated.')
+            return redirect('home')
+
+        else:
+            order.order_items.add(order_item[0])
+            messages.info(request, 'Item added to your cart.')
+            return redirect('home')
+    
+    else:
+        order = Order(user=request.user)
+        order.save()
+        order.order_items.add(order_item[0])
+        messages.info(request, 'Item add to your cart.')
+        return redirect('home')
+
+
+# view cart
+def cart(request):
+    carts = Cart.objects.filter(user=request.user, purchased=False)
+    orders = Cart.objects.filter(user=request.user, purchased=False)
+
+    if carts.exists() and orders.exists():
+        order = orders[0]
+        context = {'carts': carts, 'order': order}
+        return render(request, 'order/cart.html', context)
+
+    else:
+        messages.warning(request, "You don't have any item in your cart!")
+        return redirect('home')
