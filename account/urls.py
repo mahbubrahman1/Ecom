920890ea614bc2ec40332d2b1fcfb76de6435e5a
mahbubrahman1 <mahbuburrahman3@yahoo.com>
2022-10-28 11:45:24 +0600

    fixed some error

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,6 @@ from .import views
 urlpatterns = [
     path('registration/', views.registration, name='registration'),
     path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='slogout'),
+    path('logout/', views.user_logout, name='logout'),
     path('profile/', views.user_profile, name='profile'),
 ]
